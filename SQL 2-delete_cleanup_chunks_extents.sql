2b661687a81751a6f7e342a84a7e08847cce1228
박문기(Moon-Kee Bahk) <mkbahk@gmail.com>
2022-03-29 12:37:44 +0900

    Create SQL 2-delete_cleanup_chunks_extents.sql

---
+++ SQL 2-delete_cleanup_chunks_extents.sql
@@ -0,0 +1,43 @@
+﻿select
+  *
+from
+  countries;
+---
+insert into
+  countries
+values
+  ('KR', 'South Korea', 3);
+---
+select
+  *
+from
+  countries
+where
+  country_id = 'KR';
+---
+delete from
+  countries
+where
+  country_id = 'KR';
+---
+SELECT
+  t.table_name
+FROM
+  sqream_catalog.delete_predicates dp
+  JOIN sqream_catalog.tables t ON dp.table_id = t.table_id
+GROUP BY
+  1;
+---
+SELECT
+  delete_predicate
+FROM
+  sqream_catalog.delete_predicates dp
+  JOIN sqream_catalog.tables t ON dp.table_id = t.table_id
+WHERE
+  t.table_name = 'countries';
+---
+select cleanup_chunks('public','countries');
+---
+select * from sqream_catalog.tables where table_name = 'countries';
+---
+select cleanup_extents('public','countries');
