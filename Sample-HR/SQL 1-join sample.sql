9ebccce96e199cf7e2c1fc053117fc07d512948f
박문기(Moon-Kee Bahk) <mkbahk@gmail.com>
2022-04-01 15:30:03 +0900

    PySQream Sample

---
+++ Sample-HR/SQL 1-join sample.sql
@@ -0,0 +1,57 @@
+﻿select
+  show_version();
+
+---
+
+select
+  c.country_id,
+  c.country_name,
+  r.region_id,
+  r.region_name
+from
+  countries c,
+  regions r
+where
+  c.region_id = r.region_id
+order by
+  c.region_id,
+  c.country_id;
+
+---
+
+select
+  c.country_id,
+  c.country_name,
+  r.region_id,
+  r.region_name
+from
+  countries as c
+  inner join regions as r on c.region_id = r.region_id
+where
+  r.region_id = 1
+order by
+  c.region_id,
+  c.country_id;
+
+---
+
+select
+  c.country_id,
+  c.country_name,
+  r.region_id,
+  r.region_name
+from
+  countries as c
+  inner join (
+    select
+      region_id,
+      region_name
+    from
+      regions
+    where
+      region_id = 1
+  ) as r
+order by
+  c.region_id,
+  c.country_id;
+ ---
---
+++ Sample_pysqream.py
@@ -0,0 +1,44 @@
+import pysqream
+
+"""
+Connection parameters include:
+* IP/Hostname
+* Port
+* database name
+* username
+* password
+* Connect through load balancer, or direct to worker (Default: false - direct to worker)
+* use SSL connection (default: false)
+* Optional service queue (default: 'sqream')
+"""
+
+# Create a connection object
+
+con = pysqream.connect( host='127.0.0.1'
+			, port=3108
+			, database='humanresources'
+                   	, username='sqream'
+			, password='sqream'
+                   	, clustered=True )
+
+# Create a new cursor
+cur = con.cursor()
+
+# Prepare and execute a query
+cur.execute('select * from regions;')
+
+result = cur.fetchall() # `fetchall` gets the entire data set
+
+#print (f"Version: {result[0][0]}")
+print ("region id	", "region_name")
+
+for data in result:
+   print(data[0]," 		", data[1])
+
+# This should print the SQream DB version. For example ``Version: v2020.1``.
+# Finally, close the connection
+con.close()
+
+#
+# End of codes
+#
